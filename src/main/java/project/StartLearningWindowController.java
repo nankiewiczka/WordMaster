1b724d43d44e980f7a26f567623150c856287020
nankiewiczka <karolina.nankiewicz@gmail.com>
2018-11-02 19:57:10 +0100

    add combobox

--- src/main/java/project/StartLearningWindowController.java
+++ src/main/java/project/StartLearningWindowController.java
@@ -1,7 +1,10 @@
 package project;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
 public class StartLearningWindowController {
@@ -17,6 +20,16 @@ public class StartLearningWindowController {
     @FXML
     private Label listNameContent;
 
+    @FXML
+    private ChoiceBox selectLanguage;
+
+    ObservableList<String> options = FXCollections.observableArrayList("polski-obcy", "obcy-polski");
+
+    @FXML
+    private void initialize() {
+        selectLanguage.setValue("polski-obcy");
+        selectLanguage.setItems(options);
+    }
     @FXML
     public void startLearning() {
         listNameContent.textProperty().setValue("Ustawiamy");
