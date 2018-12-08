fbc70503726cb899e9b7a8ecca9f25a17b506f33
nankiewiczka <karolina.nankiewicz@gmail.com>
2018-12-08 22:17:35 +0100

    abstract class has now fields

--- src/main/java/project/model/LearningEntity.java
+++ src/main/java/project/model/LearningEntity.java
@@ -1,10 +1,26 @@
 package project.model;
 
+import project.checker.WordChecker;
+
 public abstract class LearningEntity {
-    public abstract boolean checkCorrectness(String input);
+    protected LearningUnit learningUnit;
+    protected LearningList wordLearningList;
+    protected WordChecker wordChecker;
+    protected Word word;
     public abstract String getWordForLabel();
-    public abstract int getWordAmount();
-    public abstract int getRemainAmount();
-    public abstract LearningUnit getLearningUnit();
+    public boolean checkCorrectness(String input) {
+        return wordChecker.checkCorrectness(word, input);
+    }
+    public int getWordAmount() {
+        return learningUnit.getWordsAmount();
+    }
+    public int getRemainAmount() {
+        return wordLearningList.remainsAmount();
+    }
+    public LearningUnit getLearningUnit() {
+        return learningUnit;
+    }
+
+
 
 }
--- src/main/java/project/model/LearningEntityFromNative.java
+++ src/main/java/project/model/LearningEntityFromNative.java
@@ -3,20 +3,11 @@ package project.model;
 import project.checker.WordForeignChecker;
 
 public class LearningEntityFromNative extends LearningEntity {
-    private LearningUnit learningUnit;
-    private LearningList wordLearningList;
-    private WordForeignChecker wordForeignChecker;
-    private Word word;
 
     public LearningEntityFromNative(LearningUnit learningUnit) {
         this.learningUnit = learningUnit;
         this.wordLearningList = new LearningList(learningUnit.getWordList().getWordsList());
-        wordForeignChecker = new WordForeignChecker();
-    }
-
-    @Override
-    public boolean checkCorrectness(String input) {
-        return wordForeignChecker.checkCorrectness(word, input);
+        wordChecker = new WordForeignChecker();
     }
 
     @Override
@@ -27,21 +18,5 @@ public class LearningEntityFromNative extends LearningEntity {
             return word.getPolishWord();
         }
         else return null;
-
-    }
-
-    @Override
-    public int getWordAmount() {
-        return learningUnit.getWordsAmount();
-    }
-
-    @Override
-    public int getRemainAmount() {
-        return wordLearningList.remainsAmount();
-    }
-
-    @Override
-    public LearningUnit getLearningUnit() {
-        return learningUnit;
     }
 }
